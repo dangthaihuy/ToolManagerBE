94b7a6ec294101e51cbddef1701f57f3a339c0ac
HaiLt <61742028+HaiLT24@users.noreply.github.com>
2022-08-22 11:05:50 +0700

    test thêm thành viên vào nhóm chat

---
+++ SourceCode/Manager/Manager.WebApp/Controllers/Business/GroupUserController.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using Manager.DataLayer.Stores.Business;
+using Manager.SharedLibs;
+using Manager.WebApp.Models.Business;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace Manager.WebApp.Controllers.Business
+{
+    [Route("api/chat/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class GroupUserController : ControllerBase
+    {
+        private readonly IStoreGroup storeGroup;
+        private readonly ILogger<GroupUserController> _logger;
+        public GroupUserController(ILogger<GroupUserController> logger)
+        {
+
+            storeGroup = Startup.IocContainer.Resolve<IStoreGroup>();
+            _logger = logger;
+
+        }
+
+        [HttpPost]
+        [Route("insert")]
+        public async Task<IActionResult> Insert(GroupUserModel model)
+        {
+            try
+            {
+                foreach(string item in model.UsersId)
+                {
+                    var Res = storeGroup.Insert(model.GroupId, Utils.ConvertToInt32(item));
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogDebug("Could not login: " + ex.ToString());
+            }
+
+            return Ok();
+        }
+
+    }
+}
--- SourceCode/Manager/Manager.WebApp/Hubs/ChatHub.cs
+++ SourceCode/Manager/Manager.WebApp/Hubs/ChatHub.cs
@@ -93,7 +93,7 @@ namespace Manager.WebApp.Hubs
                 {
                     foreach (var senderConn in fromUser.Connections)
                     {
-                        Clients.Client(senderConn.ConnectionId).SendAsync("ReceiveMessage", Conversation.Id, Utils.ConvertToInt32(SenderId), Message, DateTime.Now);
+                        Clients.Client(senderConn.ConnectionId).SendAsync("ReceiveMessage", Conversation.Id, Utils.ConvertToInt32(SenderId), Message, IdentityMessage.CreateDate);
                     }
                 }
 
@@ -102,7 +102,7 @@ namespace Manager.WebApp.Hubs
                     foreach (var receiverConn in toUser.Connections)
                     {
                         // Broad cast message
-                        Clients.Client(receiverConn.ConnectionId).SendAsync("ReceiveMessage", Conversation.Id, Utils.ConvertToInt32(SenderId), Message, DateTime.Now);
+                        Clients.Client(receiverConn.ConnectionId).SendAsync("ReceiveMessage", Conversation.Id, Utils.ConvertToInt32(SenderId), Message, IdentityMessage.CreateDate);
                     }
                 }
             }
