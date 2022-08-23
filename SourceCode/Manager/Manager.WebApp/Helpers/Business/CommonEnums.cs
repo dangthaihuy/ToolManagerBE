0725db03a6ddf243add638cccd964b1523408cd2
HaiLt <61742028+HaiLT24@users.noreply.github.com>
2022-08-23 10:47:16 +0700

    fix lại lỗi thêm thành viên

--- SourceCode/Manager/Manager.WebApp/Helpers/Business/CommonEnums.cs
+++ SourceCode/Manager/Manager.WebApp/Helpers/Business/CommonEnums.cs
@@ -8,7 +8,7 @@ namespace Manager.WebApp.Helpers.Business
     public static class EnumFormatInfoCacheKeys
     {
         public static string Conversation = "CONVERSATION_{0}_{1}";
-        public static string ConversationGroup = "CONVERSATION_{0}_GROUP_{1}";
+        public static string ConversationGroup = "CONVERSATION_GROUP_{0}";
         public static string ConversationLastMessage = "CONVERSATIONLASTMESSAGE_{0}";
         public static string GroupOfUser = "GROUPOFUSER_{0}";
     }
