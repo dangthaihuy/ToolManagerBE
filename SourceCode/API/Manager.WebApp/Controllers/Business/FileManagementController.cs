61b19d07aec2680b8ab080081c2b2955c02f8147
HaiLt <83907998+dangthaihuy@users.noreply.github.com>
2022-09-16 17:42:09 +0700

    done 16/9/2022

--- SourceCode/API/Manager.WebApp/Controllers/Business/FileManagementController.cs
+++ SourceCode/API/Manager.WebApp/Controllers/Business/FileManagementController.cs
@@ -3,12 +3,14 @@ using Manager.DataLayer.Entities.Business;
 using Manager.DataLayer.Stores.Business;
 using Manager.DataLayer.Stores.System;
 using Manager.SharedLibs;
+using Manager.WebApp.Helpers;
 using Manager.WebApp.Models.Business;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Threading.Tasks;
 
 namespace Manager.WebApp.Controllers.Business
 {
@@ -28,6 +30,9 @@ namespace Manager.WebApp.Controllers.Business
             storeFileManagement = Startup.IocContainer.Resolve<IStoreFileManagement>();
             _logger = logger;
         }
+
+
+        // XỬ LÝ FOLDER
 
         [HttpPost]
         [Route("insertfolder")]
@@ -61,8 +66,8 @@ namespace Manager.WebApp.Controllers.Business
             try
             {
                 var identity = model.MappingObject<IdentityFolder>();
-                /*var res = storeFileManagement.DeleteFolder(identity);*/
 
+                
                 DeleteChild(identity);
 
                 return Ok(new { apiMessage = new { type = "success", code = "folder002" } });
@@ -96,6 +101,52 @@ namespace Manager.WebApp.Controllers.Business
             }
         }
 
+
+
+
+        // XỬ LÝ FOLDER
+        [HttpPost]
+        [Route("insertfile")]
+        public async Task<ActionResult> InsertFile([FromForm] FileModel model)
+        {
+            try
+            {
+                var identity = model.MappingObject<IdentityFile>();
+                             
+                if (Request.Form.Files.Count > 0)
+                {
+                    foreach (var file in Request.Form.Files)
+                    {
+                        var attachmentFolder = string.Format("Files");
+                        var filePath = FileUploadHelper.UploadFile(file, attachmentFolder);
+
+                        await Task.FromResult(filePath);
+
+                        if (!string.IsNullOrEmpty(filePath))
+                        {
+                            identity.Name = file.FileName;
+                            identity.Path = filePath;
+                        }
+
+                        /*var updateFile = storeFileManagement.InsertFile(identity);*/
+                    }
+
+                    return Ok(new { apiMessage = new { type = "success", code = "file001" } });
+                }
+                    
+
+                
+            }
+            catch(Exception ex)
+            {
+                _logger.LogDebug("Could not insert file: " + ex.ToString());
+
+                return StatusCode(500, new { apiMessage = new { type = "error", code = "server001" } });
+            }
+
+            return Ok(new { apiMessage = new { type = "error", code = "file101" } });
+        }
+
         private void DeleteChild(IdentityFolder parent)
         {
             try
@@ -110,9 +161,9 @@ namespace Manager.WebApp.Controllers.Business
                     }
                 }
 
-
+                var res = storeFileManagement.DeleteFolder(parent);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 _logger.LogDebug("Could not delete child: " + ex.ToString());
 
